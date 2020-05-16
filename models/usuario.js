16a648e20727ddb44b9b204b40abe35580dd9837
Anderson <anderbabri@hotmail.com>
2020-05-16 12:25:24 -0500

    Seccion 11 terminada - CRUD - Imagenes

--- models/usuario.js
+++ models/usuario.js
@@ -13,7 +13,7 @@ var usuarioSchema = new Schema({
 
     nombre: { type: String, required: [true, 'El nombre es necesario'] },
     email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
-    password: { type: String, required: [true, 'El contraseña es necesaria'] },
+    password: { type: String, required: [true, 'La contraseña es necesaria'] },
     img: { type: String, required: false },
     role: { type: String, required: true, default: 'USER_ROLE', enum: rolesValidos },
 });
